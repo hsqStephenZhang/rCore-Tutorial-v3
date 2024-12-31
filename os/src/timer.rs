d4012ed66d9c088d5ffa6227a536158b4d173d7e
hsqStephenZhang <stephenzhang666666@gmail.com>
2024-12-31 16:17:56 +0800

    add user alloc test

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -17,6 +17,10 @@ pub fn get_time_ms() -> usize {
     time::read() / (CLOCK_FREQ / MSEC_PER_SEC)
 }
 
+pub fn get_time_us() -> usize {
+    time::read() / (CLOCK_FREQ / MSEC_PER_SEC / MSEC_PER_SEC)
+}
+
 /// set the next timer interrupt
 pub fn set_next_trigger() {
     set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
