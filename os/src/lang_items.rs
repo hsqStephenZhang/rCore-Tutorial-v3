528cb4ced9b9fac1ee5e80b7f2118f5131b56d39
hsqStephenZhang <stephenzhang666666@gmail.com>
2024-12-31 14:06:20 +0800

    fix stack trace

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,6 +1,9 @@
 //! The panic handler
 
-use crate::sbi::shutdown;
+use crate::{
+    mm::{memory_set::KERNEL_SPACE, VirtAddr},
+    sbi::shutdown,
+};
 use core::panic::PanicInfo;
 use log::*;
 
@@ -20,18 +23,34 @@ fn panic(info: &PanicInfo) -> ! {
     shutdown(true)
 }
 
-
 #[no_mangle]
 pub fn print_stack_trace() {
     let mut fp: *const usize;
     unsafe {
         core::arch::asm!("mv {}, fp", out(reg) fp);
     }
+    
+    fn is_valid(fp: *const usize) -> bool {
+        if fp == core::ptr::null() {
+            return false;
+        }
+        let ra_addr = unsafe { fp.sub(1) as usize };
+        let ra_vpn = VirtAddr::from(ra_addr).into();
+        let next_fp_addr = unsafe { fp.sub(2) as usize };
+        let next_fp_vpn = VirtAddr::from(next_fp_addr).into();
+        let kernel_space = KERNEL_SPACE.exclusive_access();
+        if kernel_space.translate(ra_vpn).is_none() || kernel_space.translate(next_fp_vpn).is_none()
+        {
+            return false;
+        }
+        true
+    }
     warn!("stack trace:");
-    while fp != core::ptr::null() {
+    while is_valid(fp) {
         let ra = unsafe { *fp.sub(1) };
         let next_fp = unsafe { *fp.sub(2) };
         warn!("fp: {:#x}, ra: {:#x}", fp as usize, ra);
         fp = next_fp as *const usize;
     }
+    warn!("stack trace done");
 }
